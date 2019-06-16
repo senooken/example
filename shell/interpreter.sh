57d7da7b5309e7258147e7f41c6537aa6c01f074
SENOO, Ken <contact@senooken.jp>
2019-06-16 14:45:00 +0900

    利点の順番を変更し，シェルがシバンを認識できない件は話が違うので削除

--- shell/interpreter.sh
+++ shell/interpreter.sh
@@ -1,4 +1,4 @@
-command -p tail -n +2 "$0" | command -p ${1?Set interpreter (ex: php, perl, python, ruby etc) to 1st argument.} ${1+"$@"}; exit $?
+command -p tail -n +2 "$0" | command -p "${1?Set interpreter (ex: php, perl, python, ruby etc) to 1st argument.}" ${1+"$@"}; exit $?
 ## \file interpreter.sh
 ## \authore SENOO, Ken
 ## \copyright CC0
@@ -30,8 +30,8 @@ command -p tail -n +2 "$0" | command -p ${1?Set interpreter (ex: php, perl, pyth
 
 ## 1. どのインタープリターでも同じ書き方で対応可能。
 ## 2. /bin/shが存在しないOS (Andorid) でも対応可能。
-## 3. command -pにより実行することで，仮にPATH環境変数が空でも実行可能性上昇。
-## 4. 2個以上の複雑な引数も引き渡し可能。
+## 3. 2個以上の複雑な引数も引き渡し可能。
+## 4. 組み込みコマンドのcommand -pにより仮にPATH環境変数が空でも実行可能性上昇。
 
 ## 逆に，欠点は以下2点だ。
 
@@ -40,10 +40,6 @@ command -p tail -n +2 "$0" | command -p ${1?Set interpreter (ex: php, perl, pyth
 
 ## 特に，2番目の欠点の影響が大きい。
 
-## 私見としては，インタープリター言語の開発には，シバンを付けないほうがよいだろう。シバンを指定する限り，移植可能性の問題を解決できない。
-
-## そもそも，[POSIX上](http://pubs.opengroup.org/onlinepubs/9699919799.2018edition/)に以下の記載がある限り，シバンが存在するとシェルの動作結果は不定となる。
-
-## > 1. The shell reads its input from a file (see sh), from the -c option or from the system() and popen() functions defined in the System Interfaces volume of POSIX.1-2017. If the first line of a file of shell commands starts with the characters "#!", the results are unspecified.
+## 私見としては，インタープリター言語の開発には，シバンを付けないほうがよいだろう。シバンを指定する限り，移植性の低下を避けられない。
 
 ## ファイル名で実行したい場合，パッケージングでバイナリー形式にして配布したほうがよいだろう。
