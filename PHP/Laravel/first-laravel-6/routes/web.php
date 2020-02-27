f2dcf90369fb2d70f9e1045b864f225e6f68650c
SENOO, Ken <contact@senooken.jp>
2020-02-27 17:01:35 +0900

    ルーティング設定
    Chapter8に従って，本の追加と削除のルーティングを設定した。

--- PHP/Laravel/first-laravel-6/routes/web.php
+++ PHP/Laravel/first-laravel-6/routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
+use App\Book;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/books', function (Request $request) {
+});
+
+Route::delete('/book/{book}', function (Book $book) {
 });
