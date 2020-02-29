7e9ece8c056eb51c24b84f28e4a6a8357e705f3a
SENOO, Ken <contact@senooken.jp>
2020-02-29 21:25:59 +0900

    Chapter11: 本の登録処理を追加

--- PHP/Laravel/first-laravel-6/routes/web.php
+++ PHP/Laravel/first-laravel-6/routes/web.php
@@ -19,6 +19,21 @@ Route::get('/', function () {
 });
 
 Route::post('/books', function (Request $request) {
+  $validator = Validator::make($request->all(), [
+    'item_name' => 'required|max:255',
+  ]);
+
+  if ($validator->fails()) {
+    return redirect('/')->withInput()->withErros($validator);
+  }
+
+  $books = new Book;
+  $books->item_name = $request->item_name;
+  $books->item_number = '1';
+  $books->item_amount = '1000';
+  $books->published = '2017-03-07 00:00:00';
+  $books->save();
+  return redirect('/');
 });
 
 Route::delete('/book/{book}', function (Book $book) {
