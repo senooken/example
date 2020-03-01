b1fdac731ef460059e99e67c2d4e805ec414dbaa
SENOO, Ken <contact@senooken.jp>
2020-03-01 17:02:52 +0900

    Chapter14: コントローラーに処理を移動

---
+++ PHP/Laravel/first-laravel-6/app/Http/Controllers/BooksController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Book;
+use Validator;
+
+class BooksController extends Controller
+{
+  // Update
+  public function update(Request $request) {
+    $validator = Validator::make($request->all(), [
+      'id' => 'required',
+      'item_name' => 'required|min:3|max:255',
+      'item_number' => 'required|min:1|max:3',
+      'item_amount' => 'required|max:6',
+      'published' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+      return redirect('/')->withInput()->withErrors($validator);
+    }
+
+    $books = Book::find($request->id);
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
+    $books->save();
+    return redirect('/');
+  }
+
+  // Register
+  public function store(Request $request) {
+    $validator = Validator::make($request->all(), [
+      'item_name' => 'required|min:3|max:255',
+      'item_number' => 'required|min:1|max:3',
+      'item_amount' => 'required|max:6',
+      'published' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+      return redirect('/')->withInput()->withErrors($validator);
+    }
+
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
+    $books->save();
+    return redirect('/');
+  }
+}
--- PHP/Laravel/first-laravel-6/routes/web.php
+++ PHP/Laravel/first-laravel-6/routes/web.php
@@ -19,26 +19,7 @@ Route::get('/', function () {
   return view('books', ['books' => $books]);
 });
 
-Route::post('/books', function (Request $request) {
-  $validator = Validator::make($request->all(), [
-    'item_name' => 'required|min:3|max:255',
-    'item_number' => 'required|min:1|max:3',
-    'item_amount' => 'required|max:6',
-    'published' => 'required|max:255',
-  ]);
-
-  if ($validator->fails()) {
-    return redirect('/')->withInput()->withErrors($validator);
-  }
-
-  $books = new Book;
-  $books->item_name = $request->item_name;
-  $books->item_number = $request->item_number;
-  $books->item_amount = $request->item_amount;
-  $books->published = $request->published;
-  $books->save();
-  return redirect('/');
-});
+Route::post('/books', 'BooksController@store');
 
 Route::delete('/book/{book}', function (Book $book) {
   $book->delete();
@@ -52,28 +33,7 @@ Route::post('/booksedit/{books}', function(Book $books) {
 });
 
 // 更新処理
-Route::post('/books/update', function(Request $request) {
-  $validator = Validator::make($request->all(), [
-    'id' => 'required',
-    'item_name' => 'required|min:3|max:255',
-    'item_number' => 'required|min:1|max:3',
-    'item_amount' => 'required|max:6',
-    'published' => 'required',
-  ]);
-
-  if ($validator->fails()) {
-    return redirect('/')->withInput()->withErrors($validator);
-  }
-
-  // Update date.
-  $books = Book::find($request->id);
-  $books->item_name = $request->item_name;
-  $books->item_number = $request->item_number;
-  $books->item_amount = $request->item_amount;
-  $books->published = $request->published;
-  $books->save();
-  return redirect('/');
-});
+Route::post('/books/update', 'BooksController@update');
 
 Auth::routes();
 
