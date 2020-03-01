5fc2c6fb9dd70b16ca9b1fcb04655f53d0719373
SENOO, Ken <contact@senooken.jp>
2020-03-01 18:25:58 +0900

    ユーザー認証機能を追加し，ユーザーごとに表示内容を変更

--- PHP/Laravel/first-laravel-6/app/Http/Controllers/BooksController.php
+++ PHP/Laravel/first-laravel-6/app/Http/Controllers/BooksController.php
@@ -7,33 +7,17 @@ use Illuminate\Http\Request;
 use App\Book;
 use Validator;
 
+use Auth;
+
 class BooksController extends Controller
 {
   public function __construct() {
     $this->middleware('auth');
   }
 
-  // Update
-  public function update(Request $request) {
-    $validator = Validator::make($request->all(), [
-      'id' => 'required',
-      'item_name' => 'required|min:3|max:255',
-      'item_number' => 'required|min:1|max:3',
-      'item_amount' => 'required|max:6',
-      'published' => 'required',
-    ]);
-
-    if ($validator->fails()) {
-      return redirect('/')->withInput()->withErrors($validator);
-    }
-
-    $books = Book::find($request->id);
-    $books->item_name = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->published = $request->published;
-    $books->save();
-    return redirect('/');
+  public function index() {
+    $books = Book::where('user_id', Auth::user()->id)->orderBy('created_at', 'asc')->paginate(3);
+    return view('books', ['books' => $books]);
   }
 
   // Register
@@ -50,6 +34,7 @@ class BooksController extends Controller
     }
 
     $books = new Book;
+    $books->user_id = Auth::user()->id;
     $books->item_name = $request->item_name;
     $books->item_number = $request->item_number;
     $books->item_amount = $request->item_amount;
@@ -58,13 +43,32 @@ class BooksController extends Controller
     return redirect('/');
   }
 
-  public function index() {
-    $books = Book::orderBy('created_at', 'asc')->paginate(3);
-    return view('books', ['books' => $books]);
+  public function edit(Books $book_id) {
+    $books = Book::where('user_id', Auth::user()->id)->find($book_id);
+    return view('booksedit', ['book' => $books]);
   }
 
-  public function edit(Books $books) {
-    return view('booksedit', ['book' => $books]);
+  // Update
+  public function update(Request $request) {
+    $validator = Validator::make($request->all(), [
+      'id' => 'required',
+      'item_name' => 'required|min:3|max:255',
+      'item_number' => 'required|min:1|max:3',
+      'item_amount' => 'required|max:6',
+      'published' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+      return redirect('/')->withInput()->withErrors($validator);
+    }
+
+    $books = Book::where('user_id', Auth::user()->id)->find($request->id);
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
+    $books->save();
+    return redirect('/');
   }
 
   public function destroy(Book $book) {
