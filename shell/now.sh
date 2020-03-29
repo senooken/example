ada1b3015316c7663ed215fae0e614f8f8dd4b57
SENOO, Ken <contact@senooken.jp>
2020-03-29 12:29:23 +0900

    Fix structure

--- shell/now.sh
+++ shell/now.sh
@@ -3,26 +3,27 @@
 ## \file      now.sh
 ## \author    SENOO, Ken
 ## \copyright CC0
-## \version   0.1.1
+## \version   0.2.0
 ## \date      Created: 2016-12-27T23:46+09:00
-## \date      Updated: 2020-03-29T12:22+09:00
+## \date      Updated: 2020-03-29T12:30+09:00
 ## \sa        https://senooken.jp/post/2016/12/27/
-## \brief     Get current time in POSIX shell script.
+## \brief     Current date time in POSIX shell script.
 ################################################################################
 
+EXE_NAME='now'
+
 now() {
-	EXE_NAME='now'
 	dt=$(date +%Y%m%dT%H%M%S)
 	OPTSTR=':hlst-:'
 	for opt in $(echo $OPTSTR | sed 's/[:-]//g' | fold -w 1); do
-		eval is_opt_$opt='false'
+		eval opt_$opt='false'
 	done
 
 	while getopts $OPTSTR opt; do
 		case "$opt${OPTARG:-}" in
-			l|-long)  is_opt_l='true' dt=$(date +%Y-%m-%dT%H:%M:%S);;
+			l|-long)  opt_l='true' dt=$(date +%Y-%m-%dT%H:%M:%S);;
 			s|-short) dt=$(date +%Y%m%dT%H%M%S);;
-			t|-time-zone) is_opt_t='true';;
+			t|-time-zone) opt_t='true';;
 			h|-help)
 				cat <<-EOT
 Usage: $EXE_NAME [OPTION]...
@@ -39,7 +40,7 @@ Print current date time with ISO 8601 format (YYYYmmddThhmmss).
 		esac
 	done
 
-	if $is_opt_t; then
+	if $opt_t; then
 		EXE_DIR=$(dirname "$0")
 		[ -r "$EXE_DIR/tz.sh" ] && . "$EXE_DIR/tz.sh"
 		command -v tz.sh >/dev/null && . tz.sh
@@ -48,11 +49,27 @@ Print current date time with ISO 8601 format (YYYYmmddThhmmss).
 			exit 1
 		fi
 		TIME_ZONE=$(tz)
-		$is_opt_l && TIME_ZONE=$(echo "$TIME_ZONE" | sed 's/://')
+		! $opt_l && TIME_ZONE=$(echo "$TIME_ZONE" | sed 's/://')
 		dt="$dt$TIME_ZONE"
 	fi
 
 	echo "$dt"
 }
 
-now ${1+"$@"}
+main() {
+	## \brief Initialize POSIX shell environment
+	init() {
+		PATH="$(command -p getconf PATH 2>&-):${PATH:-.}"
+		export PATH="${PATH#:}" LC_ALL='C'
+		umask 0022
+		set -eu
+	}
+
+	is_main() { [ "$EXE_NAME.sh" = "${0##*/}" ]; }
+	if is_main; then
+		init
+		$EXE_NAME ${1+"$@"}
+	fi
+}
+
+main ${1+"$@"}
