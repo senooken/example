2382b4105da4dc1e75542d75326ad92dc4c2e8ff
SENOO, Ken <contact@senooken.jp>
2020-03-29 09:54:00 +0900

    Rename get_tz to tz

--- shell/tz.sh
+++ shell/tz.sh
@@ -1,16 +1,16 @@
 :
 ################################################################################
-## \file      get_tz.sh
+## \file      tz.sh
 ## \author    SENOO, Ken
 ## \copyright CC0
-## \sa        https://senooken.jp/post/2016/12/27/
-## \version   0.0.1
+## \version   0.1.0
 ## \date      Created: 2016-12-27T23:46+09:00
-## \date      Updated: 2020-03-29T09:51+09:00
+## \date      Updated: 2020-03-29T09:55+09:00
+## \sa        https://senooken.jp/post/2016/12/27/
 ## \brief     Get time zone in POSIX shell script.
 ################################################################################
 
-get_tz()(
+tz()(
 	set $(date -u '+%Y %j %H %M'); U_Y=$1 U_D=${2#0} U_D=${U_D#0} U_H=${3#0} U_M=${4#0}
 	set $(date    '+%Y %j %H %M'); L_Y=$1 L_D=${2#0} L_D=${L_D#0} L_H=${3#0} L_M=${4#0}
 
@@ -28,4 +28,4 @@ get_tz()(
 	printf '%+03d:%+03d\n' $((dm / 60)) $((dm % 60)) | sed 's/:[+-]/:/'
 )
 
-get_tz
+tz
