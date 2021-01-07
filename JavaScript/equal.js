05a01f7af74213ada081384a79362281be7581dd
SENOO, Ken <contact@senooken.jp>
2021-01-07 21:15:26 +0900

    余分な処理を削除

--- JavaScript/equal.js
+++ JavaScript/equal.js
@@ -16,22 +16,18 @@
  * @return {boolean} True if both of objects are same value, otherwise false.
  */
 function equal(object1, object2) {
-  if (typeof object1 !== 'object' || typeof object2 !== 'object') {
+  if (object1 === object2) {
+    return true;
+  } else if (typeof object1 !== 'object' || typeof object2 !== 'object') {
     return object1 === object2;
   } else if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } else if (object1 === object2) {
-    return true;
   }
   for (const key in object1) {
     if (!(key in object2)) {
       return false;
     }
-    if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
-      if (!this.equal(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
+    if (!this.equal(object1[key], object2[key])) {
       return false;
     }
   }
